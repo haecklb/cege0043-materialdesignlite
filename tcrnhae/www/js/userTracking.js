954220d5021b4cc2eae6c35e113bc8a58281c048
Brianna Haeckl <breee.haeckl@gmail.com>
2019-02-28 12:08:25 -0500

    Test adding functionality to Track Location button added in left-hand menu

--- tcrnhae/www/js/userTracking.js
+++ tcrnhae/www/js/userTracking.js
@@ -5,10 +5,9 @@ function trackLocation(){
 		document.getElementById('showLocation').innerHTML="Geolocation is not supported by this browser.";
 	}
 }
+
 function showPosition(position){
-	document.getElementById('showLocation').innerHTML="Latitude:"+position.coords.latitude+"<br>Longitude:"+position.coords.longitude;
-	L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap)
-		.bindPopup("<b>You were here</b>");
+	L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap).bindPopup("<b>You were here</b>");
 	//change the map zoom so that all the data is shown
 	//mymap.fitBounds(L.marker.getBounds());
 }
