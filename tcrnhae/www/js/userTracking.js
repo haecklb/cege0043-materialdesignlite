a9aaa0c338ac698e6f47067a2dce1addb4758c25
Brianna Haeckl <breee.haeckl@gmail.com>
2019-02-28 12:38:57 -0500

    Changes the default view of the map to the user's location when 'Track Location' is clicked

--- tcrnhae/www/js/userTracking.js
+++ tcrnhae/www/js/userTracking.js
@@ -3,11 +3,10 @@ function trackLocation(){
 		navigator.geolocation.watchPosition(showPosition);
 	} else {
 		document.getElementById('showLocation').innerHTML="Geolocation is not supported by this browser.";
-	}
-}
+			}
+		}
 
 function showPosition(position){
 	L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap).bindPopup("<b>You were here</b>");
-	//change the map zoom so that all the data is shown
-	mymap.fitBounds(L.marker.getBounds());
+	mymap.setView([position.coords.latitude, position.coords.longitude], 13)
 }
