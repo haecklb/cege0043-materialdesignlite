72965b4e3696350e43fb282ea33bd82e3844efab
Brianna Haeckl <breee.haeckl@gmail.com>
2019-02-28 08:27:44 -0500

    Test adding point, line, polygon to the map

---
+++ tcrnhae/platforms/browser/www/js/leafletFunctions.js
@@ -0,0 +1,17 @@
+function addPointLinePoly(){
+	//add a point
+	L.marker([51.5, -0.09]).addTo(mymap);
+	//add a line
+	L.polyline([[51.5, -0.09], [51.509, -0.08]]).addTo(mymap);
+	//add a polygon with 3 end points(i.e. a triangle)
+	L.polygon([
+		[51.509, -0.08],
+		[51.503, -0.06],
+		[51.51, -0.047]
+	],{
+		color:'red',
+		fillColor:'#f03',
+		fillOpacity: 0.5
+	}).addTo(mymap).bindPopup("I am a polygon.");
+
+}
