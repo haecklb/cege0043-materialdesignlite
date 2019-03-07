7b1a362509bb481e0b4abf2f1a930f7525acbfaa
Brianna Haeckl <breee.haeckl@gmail.com>
2019-03-07 07:07:13 -0500

    Fix error in utilities.js

--- tcrnhae/www/js/utilities.js
+++ tcrnhae/www/js/utilities.js
@@ -5,7 +5,7 @@ function getPort(){
 	var xhr=new XMLHttpRequest();
 	xhr.addEventListener("load", function(){
 		var parser=new DOMParser();
-		var doc=parser.parseFromString(xhr.response.Text, "application/xml");
+		var doc=parser.parseFromString(xhr.responseText, "application/xml");
 		httpPortNumber=doc.getElementsByTagName("node-port-http").item(0).textContent;
 		httpsPortNumber=doc.getElementsByTagName("node-port-https").item(0).textContent;
 			alert("Port: " + httpPortNumber);
@@ -17,5 +17,5 @@ function getPort(){
 	var configLocation="res/port.xml";
 	xhr.open("get", configLocation, true);
 	xhr.send();
-	
+
 }
