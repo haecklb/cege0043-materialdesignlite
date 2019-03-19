4c6bfcfe2154425b43a5871ecaae5abf97e6e831
Brianna Haeckl <breee.haeckl@gmail.com>
2019-03-18 21:41:28 -0400

    Add code from studentServer repo (branch formupload) in order to reflect data from post request

--- tcrnhae/www/js/studentServer.js
+++ tcrnhae/www/js/studentServer.js
@@ -3,6 +3,7 @@ var express=require('express');
 var path=require("path");
 var app=express();
 
+
 //add an http server to serve files to the Edge browser
 //due to certificate issues it rejects the https files if they are not
 //directly called in a typed URL
@@ -12,6 +13,57 @@ httpServer.listen(4480);
 
 app.get('/',function(req,res){
 	res.send("hello world from the HTTP server");
+});
+
+
+var bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
+
+var fs=require('fs');
+var pg=require('pg');
+
+var configtext=""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+
+//now convert the configuration file into the correct format -i.e. a name/value pair array
+var configarray=configtext.split(",");
+var config={};
+for (var i=0; i<configarray.length; i++){
+	var split=configarray[i].split(':');
+	config[split[0].trim()]=split[1].trim();
+}
+
+var pool=new pg.Pool(config);
+
+
+app.get('/postgistest', function(req,res){
+	pool.connect(function(err,client,done){
+		if(err){
+			console.log("not able to get connection "+ err);
+			res.status(400).send(err);
+		}
+		client.query('SELECT name FROM london_poi' ,function(err,result){
+			done();
+			if(err){
+				console.log(err);
+				res.status(400).send(err);
+			}
+			res.status(200).send(result.rows);
+		});
+	});
+});
+
+
+app.post('/reflectData', function(req,res){
+	//note that we are using POST here as we are uploading data
+	//so the parameters form part of the BODY of the request rather
+	//than the RESTful API
+	console.dir(req.body);
+
+	//for now, just echo the request back to the client
+	res.send(req.body);
 });
 
 
